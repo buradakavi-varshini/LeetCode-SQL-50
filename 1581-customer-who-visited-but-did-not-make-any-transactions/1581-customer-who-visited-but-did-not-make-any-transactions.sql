6e2488520f4f52507b7b0a4a98cefba679228dd4
Durga Sri Varshini Buradakavi <buradakavivarshini@gmail.com>
2026-09-16 22:05:39 +0530

    Time: 2948 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+SELECT Visits.customer_id,
+       COUNT(*) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions 
+ON Transactions.visit_id = Visits.visit_id
+WHERE transaction_id IS NULL
+GROUP BY  customer_id ;
